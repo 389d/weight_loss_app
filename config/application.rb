f6e388fedcd33c398564b8edffd3f67b1e600314
Anastasia <66307985+389d@users.noreply.github.com>
2023-12-21 07:38:38 +0300

    updet

--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WeightLossApp
+module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
-
-    # Please, add to the `ignore` list any other `lib` subdirectories that do
-    # not contain `.rb` files, or that should not be reloaded or eager loaded.
-    # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -23,5 +18,7 @@ module WeightLossApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.system_tests = nil
+    config.i18n.default_locale = :ru
   end
 end
