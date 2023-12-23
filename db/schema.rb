a2c6b8114feabc3b2f468be5449b6cf163190495
Anastasia <66307985+389d@users.noreply.github.com>
2023-12-23 22:07:50 +0300

    WQ

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_101041) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_20_024048) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "chat_messages", force: :cascade do |t|
+    t.integer "client_id"
+    t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_messages_on_client_id"
+  end
+
+  create_table "shares", force: :cascade do |t|
+    t.string "name"
     t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.decimal "cost"
+    t.string "owner"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.string "email", limit: 150, null: false
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "shares", column: "client_id"
 end
