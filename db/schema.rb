86dce63de29c6323dcf8a7156d10f944e79c2b32
Anastasia <66307985+389d@users.noreply.github.com>
2024-01-12 17:08:36 +0300

    rerer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_20_024048) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_20_024048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
@@ -44,13 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_20_024048) do
     t.string "first_name"
     t.string "last_name"
     t.string "email", limit: 150, null: false
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
